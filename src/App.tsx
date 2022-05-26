285212e7524a8d1bec0accf00d2440b600281972
stgeorgiev7 <stgeorgiev7@gmail.com>
2022-05-26 20:22:25 +0300

    fix: remove context

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,43 @@
-import { useContext, useEffect } from "react";
-import { Context } from "./_app";
-import { AppContext } from "./_app";
+import { IQuestion } from "./types/questions";
+import { IUser } from "./types/user";
+import {useState, useEffect} from "react";
 
 function App() {
- Context();
- //  const { questions, users } = useContext(AppContext);
+ const url = "https://628f2b72dc478523653aa33e.mockapi.io/";
+ const [questions, setQuestions] = useState<IQuestion[] | null>(null);
+ const [users, setUsers] = useState<IUser[] | null>(null);
 
- //  useEffect(() => {
- //   console.log("questions", questions);
- //   console.log("users", users);
- //  }, [questions, users]);
+ useEffect(() => {
+  const getQuestions = async () => {
+   const resp = await fetch(url + "/questions");
+   if (resp.ok === true) {
+    const questionsData: IQuestion[] = await resp.json();
+    setQuestions(questionsData);
+   } else {
+    throw new Error("Failed to fetch questions");
+   }
+  };
 
- return <div className="App">hey there</div>;
+  const getUsers = async () => {
+   const resp = await fetch(url + "/users");
+   if (resp.ok === true) {
+    const usersData: IUser[] = await resp.json();
+    setUsers(usersData);
+   } else {
+       throw new Error("Failed to fetch users")
+   }
+  };
+  getQuestions();
+  getUsers();
+  console.log("questions", questions);
+  console.log("users", users)
+ }, []);
+
+
+ return <div className="App">
+    
+
+ </div>;
 }
 
 export default App;
