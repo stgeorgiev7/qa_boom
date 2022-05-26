95b876a0526e6cf8cd4189793e636721dab4fb50
Stoyan Georgiev <stoyan.georgiev@boom.dev>
2022-05-26 16:57:57 +0300

    feat: fetch data from api and add it to context

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,16 @@
+import { useContext, useEffect } from "react";
+import { Context } from "./_app";
+import { AppContext } from "./_app";
+
 function App() {
+ Context();
+ //  const { questions, users } = useContext(AppContext);
+
+ //  useEffect(() => {
+ //   console.log("questions", questions);
+ //   console.log("users", users);
+ //  }, [questions, users]);
+
  return <div className="App">hey there</div>;
 }
 
