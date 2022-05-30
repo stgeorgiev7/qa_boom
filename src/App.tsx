2b3b8655cad6f56c7dab14b59e932412623d0d96
stgeorgiev7 <stgeorgiev7@gmail.com>
2022-05-30 08:37:15 +0300

    feat: created notification component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { IUser } from "./types/user";
 import { useState, useEffect } from "react";
 import styles from "./styles/App.module.scss";
 import Cards from "./components/Cards";
+import Notification from "./components/Notification";
 
 function App() {
  return (
   <div className={styles.app}>
    <div className={styles.wrapper}>
+    <Notification type='newAnswer' />
     <Cards />
    </div>
   </div>
